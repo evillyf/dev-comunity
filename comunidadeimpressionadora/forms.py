7a9dfc6bb59015f0b258e769edbc897942681c10
Evilly <sandydias9840@gmail.com>
2022-02-17 19:32:26 -0300

    Back-end completo

--- comunidadeimpressionadora/forms.py
+++ comunidadeimpressionadora/forms.py
@@ -11,14 +11,14 @@ class FormCadastro(FlaskForm):
     username = StringField('Username', validators=[DataRequired(25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     senha= PasswordField('Password', validators=[DataRequired(), Length(6, 20)])
-    confirmacao_senha = PasswordField('Confirme Password', validators=[DataRequired(), EqualTo('senha')])
+    confirmacao_senha = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_cadastro = SubmitField('Create account')
 
     #se quiser fazer o username ser único, criar outra função dessa com validade_username
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
         if usuario:
-            raise ValidationError('E-mail já cadastrado. Cadastre-se com outro e-mail ou faça login para continuar.')
+            raise ValidationError('An account is already registered with this email address.')
 
 
 class FormLogin(FlaskForm):
