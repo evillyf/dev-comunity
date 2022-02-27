81ede3d61bd7e9c46c4405140f67fbf416f94845
evilly <61359076+evillyf@users.noreply.github.com>
2022-02-26 23:24:55 -0300

    descricao

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 #segurança contra ataques pelos forms
 app.config['SECRET_KEY'] = '71234572572fbed270edec08df36c083'  #no terminal: python  >  import secrets > secrets.token_hex(16) >exit()
 #banco de dados
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pssilyckwdjvns:c757559a90b70b0a5f5ff92432c1e984c2a5c370623bf148cb964a21c59bbf9b@ec2-3-227-195-74.compute-1.amazonaws.com:5432/d1e2hkl27efku8'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
